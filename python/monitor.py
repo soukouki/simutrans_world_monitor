e44f477c1b1684d4846471780bca31af3509bfae
sou7 <soukouki0@yahoo.co.jp>
2022-07-05 22:59:25 +0900

    テストしてないけど多分動くやろ

--- python/monitor.py
+++ python/monitor.py
@@ -4,9 +4,11 @@
 import discord
 import os
 import config
+import re
 from watchdog.observers import Observer
 from libs.FileChangeHandler import FileChangeHandler
 from libs.vars import *
+from urllib import request
 
 FILE_CMD = config.DIRECTORY + '/file_io/cmd.txt'
 
@@ -25,7 +27,14 @@ async def on_message(message):
     content = message.content.replace('？','?').replace('，',',').replace('、',',')
     if message.author.bot or message.channel != channel or content[0]!='?' or len(content)<2:
         return
-            
+    
+    if content == 'IP' || content == 'ip':
+        # 適当にググって出てきたサービスを使ってるだけなので、サービス終了してたらよしなに切り替えてください
+        with request.urlopen('https://globalip.me') as req:
+            html = req.read().decode()
+        ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', html).group()
+        await channel.send(ip)
+        return
     with open(FILE_CMD, encoding='utf-8') as f:
         s = f.read()
         if s and not s.startswith('empty'):
@@ -35,7 +44,7 @@ async def on_message(message):
         f.write(content[1:])
         set_waiting_message(await channel.send(config.TEXT_WAIT))
         set_prev_out_hash(None)
-        
+    
 def generate_io_files():
     os.makedirs(config.DIRECTORY+'/file_io', exist_ok=True)
     if not os.path.isfile(FILE_CMD):
